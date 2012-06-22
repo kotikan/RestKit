9d26824ce491ee72c38392cbc802d72f5799b1d3
Alex Bird <alex@kotikan.com>
2012-06-22 11:07:07 +0100

    Added restkit static library target (copy of normal target with export of all headers except test/json etc)

---
+++ RestKitStaticLibrary/RestKitStaticLibrary.h
@@ -0,0 +1,13 @@
+//
+//  RestKitStaticLibrary.h
+//  RestKitStaticLibrary
+//
+//  Created by Alex Bird on 22/06/2012.
+//  Copyright (c) 2012 Kotikan LTD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RestKitStaticLibrary : NSObject
+
+@end
---
+++ RestKitStaticLibrary/RestKitStaticLibrary.m
@@ -0,0 +1,13 @@
+//
+//  RestKitStaticLibrary.m
+//  RestKitStaticLibrary
+//
+//  Created by Alex Bird on 22/06/2012.
+//  Copyright (c) 2012 Kotikan LTD. All rights reserved.
+//
+
+#import "RestKitStaticLibrary.h"
+
+@implementation RestKitStaticLibrary
+
+@end
